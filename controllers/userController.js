3ab55cd71ac2953d426e7ea1d125f1e4ebd5346e
mbriese <mindi.briese@gmail.com>
2020-02-29 15:31:42 -0800

    rolling back to login before promises

---
+++ controllers/userController.js
@@ -0,0 +1,27 @@
+const User = require('../models/User')
+
+exports.login = function(req, res) {
+   let user = new User(req.body)
+   user.login(function(result){
+      res.send(result)
+   })
+}
+
+exports.logout = function() {
+  
+}
+
+exports.register = function(req, res) {
+  console.log(req.body)
+  let user = new User(req.body)
+  user.register()
+  if(user.errors.length){
+     res.send(user.errors)
+  } else{
+    res.send("Congrats, no errors detected!")
+  }
+}
+
+exports.home = function(req, res) {
+  res.render('home-guest')
+}
---
+++ models/User.js
@@ -0,0 +1,58 @@
+const usersCollection = require('../db').collection("users")
+const validator = require("validator")
+
+let User = function(data) {
+  this.data = data
+  this.errors = []
+}
+
+User.prototype.cleanUp = function() {
+  if (typeof(this.data.username) != "string") {
+     this.data.username = ""}
+  if (typeof(this.data.email) != "string") {
+      this.data.email = ""}
+  if (typeof(this.data.password) != "string") {
+        this.data.password = ""}
+
+  // get rid of bogus return values by only accepting correct properties
+  this.data = {
+    username: this.data.username.trim().toLowerCase(),
+    email: this.data.email.trim().toLowerCase(),
+    password: this.data.password
+  }
+}
+
+User.prototype.validate = function() {
+  if (this.data.username == "") {this.errors.push("You must provide a username.")}
+  if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) {this.errors.push("Username can only contain letters and numbers.")}
+  if (!validator.isEmail(this.data.email)) {this.errors.push("You must provide a valid email address.")}
+  if (this.data.password == "") {this.errors.push("You must provide a password.")}
+  if (this.data.password.length > 0 && this.data.password.length < 12) {this.errors.push("Password must be at least 12 characters.")}
+  if (this.data.password.length > 100) {this.errors.push("Password cannot exceed 100 characters.")}
+  if (this.data.username.length > 0 && this.data.username.length < 3) {this.errors.push("Username must be at least 3 characters.")}
+  if (this.data.username.length > 30) {this.errors.push("Username cannot exceed 30 characters.")}
+}
+
+User.prototype.login = function(callback){
+  this.cleanUp()
+  usersCollection.findOne({username: this.data.username}, (err, attemptedUser)=>{
+    if(attemptedUser && attemptedUser.password == this.data.password){
+       callback("Congrats!")
+    } else {
+       callback("invalid username/password")
+    }
+  })
+}
+
+User.prototype.register = function() {
+  // Step 1 validate user data
+  this.cleanUp()
+  this.validate()
+  // Step 2 only if there are no validation errors
+  // store in a database
+  if (!this.errors.length) {
+    usersCollection.insertOne(this.data)
+  }
+}
+
+module.exports = User
