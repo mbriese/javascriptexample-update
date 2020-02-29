3067e62173530bae22368ba007e1bb5bf870b90a
mbriese <mindi.briese@gmail.com>
2020-02-29 15:56:01 -0800

    fixed db.js connection

--- db.js
+++ db.js
@@ -1,12 +1,9 @@
 const dotenv = require('dotenv')
 dotenv.config()
-
 const mongodb = require('mongodb')
 
-const connectionString = 'mongodb+srv://todoAppUser:Il0v3J03y@cluster0-8rhuk.mongodb.net/complexapp?retryWrites=true&w=majority'
-
-mongodb.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client){
-    module.exports = client.db()
-    const app = require('./app')
-    app.listen(process.env.PORT)
-  })
+mongodb.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
+  module.exports = client.db()
+  const app = require('./app')
+  app.listen(process.env.PORT)
+})
