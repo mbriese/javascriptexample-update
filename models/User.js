5812455c47eabfc4588e6b1b5d254faa447741fe
mbriese <mindi.briese@gmail.com>
2020-03-03 11:15:28 -0800

    adding avatar function to model

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcryptjs")
 const usersCollection = require('../db').db().collection("users")
 const validator = require("validator")
+const md5 = require('md5')
 
 let User = function(data) {
   this.data = data
@@ -56,6 +57,7 @@ User.prototype.login = function() {
     this.cleanUp()
     usersCollection.findOne({username: this.data.username}).then((attemptedUser) => {
       if (attemptedUser && bcrypt.compareSync(this.data.password, attemptedUser.password)) {
+        this.getAvatar()
         resolve("Congrats!")
       } else {
         reject("Invalid username / password.")
@@ -71,7 +73,6 @@ User.prototype.register = function(){
     // Step #1: Validate user data
     this.cleanUp()
     await this.validate()
-  
     // Step #2: Only if there are no validation errors 
     // then save the user data into a database
     if (!this.errors.length) {
@@ -79,6 +80,7 @@ User.prototype.register = function(){
       let salt = bcrypt.genSaltSync(10)
       this.data.password = bcrypt.hashSync(this.data.password, salt)
       await usersCollection.insertOne(this.data)
+      this.getAvatar()
       resolve()
     } else {
       reject(this.errors)
@@ -87,7 +89,7 @@ User.prototype.register = function(){
 }
 
 User.prototype.getAvatar = function(){
-  this.avatar = `https://gravatar.com/avatar/email?s=128`
+  this.avatar = `https://gravatar.com/avatar/${md5(this.data.email)}?s=128`
 }
 
 module.exports = User
