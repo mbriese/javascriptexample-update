73b6f3e4f134700ac00abbe90dafde772ff6ed5d
mbriese <mindi.briese@gmail.com>
2020-03-03 20:26:33 -0800

    added posts

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,20 @@
 const User = require('../models/User')
 
+exports.mustBeLoggedIn = function(req, res, next) {
+  if (req.session.user){
+    next()
+  } else {
+    req.flash("errors", "You must be logged in to perform that action")
+    req.session.save(function(){
+      res.redirect('/')
+    })
+  }
+}
+
 exports.login = function(req, res) {
   let user = new User(req.body)
   user.login().then(function(result) {
-    req.session.user = {favColor: "blue", username: user.data.username}
+    req.session.user = {avatar: user.avatar, username: user.data.username}
     req.session.save(function() {
       res.redirect('/')
     })
@@ -25,7 +36,7 @@ exports.register = function(req, res) {
   console.log(req.body)
   let user = new User(req.body)
   user.register().then(()=>{
-     req.session.user = {username: user.data.username}
+     req.session.user = {username: user.data.username, avatar: user.avatar}
      req.session.save(function(){
       res.redirect('/')
     })
@@ -41,7 +52,7 @@ exports.register = function(req, res) {
 
 exports.home = function(req, res) {
   if (req.session.user) {
-    res.render('home-dashboard', {username: req.session.user.username})
+    res.render('home-dashboard', {username: req.session.user.username, avatar: req.session.user.avatar})
   } else {
     res.render('home-guest', {errors: req.flash('errors'), regErrors: req.flash('regErrors')})
   }
