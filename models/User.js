67397a63bdd130aa8c0718c81b41e7fcaae1ce12
mbriese <mindi.briese@gmail.com>
2020-03-08 20:04:43 -0700

    user profile section

--- models/User.js
+++ models/User.js
@@ -11,14 +11,11 @@ let User = function(data, getAvatar) {
 }
 
 User.prototype.cleanUp = function() {
-  if (typeof(this.data.username) != "string") {
-     this.data.username = ""}
-  if (typeof(this.data.email) != "string") {
-      this.data.email = ""}
-  if (typeof(this.data.password) != "string") {
-        this.data.password = ""}
+  if (typeof(this.data.username) != "string") {this.data.username = ""}
+  if (typeof(this.data.email) != "string") {this.data.email = ""}
+  if (typeof(this.data.password) != "string") {this.data.password = ""}
 
-  // get rid of bogus return values by only accepting correct properties
+  // get rid of any bogus properties
   this.data = {
     username: this.data.username.trim().toLowerCase(),
     email: this.data.email.trim().toLowerCase(),
@@ -26,8 +23,8 @@ User.prototype.cleanUp = function() {
   }
 }
 
-User.prototype.validate = function(){
-  return new Promise(async (resolve, reject)=>{
+User.prototype.validate = function() {
+  return new Promise(async (resolve, reject) => {
     if (this.data.username == "") {this.errors.push("You must provide a username.")}
     if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) {this.errors.push("Username can only contain letters and numbers.")}
     if (!validator.isEmail(this.data.email)) {this.errors.push("You must provide a valid email address.")}
@@ -37,22 +34,20 @@ User.prototype.validate = function(){
     if (this.data.username.length > 0 && this.data.username.length < 3) {this.errors.push("Username must be at least 3 characters.")}
     if (this.data.username.length > 30) {this.errors.push("Username cannot exceed 30 characters.")}
   
-    // After validation - check for unique username
+    // Only if username is valid then check to see if it's already taken
     if (this.data.username.length > 2 && this.data.username.length < 31 && validator.isAlphanumeric(this.data.username)) {
       let usernameExists = await usersCollection.findOne({username: this.data.username})
-      if (usernameExists) {this.errors.push("Username is already in use")}
+      if (usernameExists) {this.errors.push("That username is already taken.")}
     }
   
-      // After validation - check for unique email
+      // Only if email is valid then check to see if it's already taken
       if (validator.isEmail(this.data.email)) {
         let emailExists = await usersCollection.findOne({email: this.data.email})
-        if (emailExists) {this.errors.push("Email is already in use")}
+        if (emailExists) {this.errors.push("That email is already being used.")}
       }
       resolve()
-  }
-  ) 
+  }) 
 }
-
 
 User.prototype.login = function() {
   return new Promise((resolve, reject) => {
@@ -71,8 +66,8 @@ User.prototype.login = function() {
   })
 }
 
-User.prototype.register = function(){
-  return new Promise(async (resolve, reject)=> {
+User.prototype.register = function() {
+  return new Promise(async (resolve, reject) => {
     // Step #1: Validate user data
     this.cleanUp()
     await this.validate()
@@ -91,8 +86,32 @@ User.prototype.register = function(){
   })
 }
 
-User.prototype.getAvatar = function(){
-  this.avatar = `https://s.gravatar.com/avatar/${md5(this.data.email)}?s=128`
+User.prototype.getAvatar = function() {
+  this.avatar = `https://gravatar.com/avatar/${md5(this.data.email)}?s=128`
+}
+
+User.findByUsername = function(username) {
+  return new Promise(function(resolve, reject) {
+    if (typeof(username) != "string") {
+      reject()
+      return
+    }
+    usersCollection.findOne({username: username}).then(function(userDoc) {
+      if (userDoc) {
+        userDoc = new User(userDoc, true)
+        userDoc = {
+          _id: userDoc.data._id,
+          username: userDoc.data.username,
+          avatar: userDoc.avatar
+        }
+        resolve(userDoc)
+      } else {
+        reject()
+      }
+    }).catch(function() {
+       reject()
+    })
+  })
 }
 
 module.exports = User
