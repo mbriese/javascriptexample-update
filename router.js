3de38f90f68cfeb734bf9726a01d64b5e3fb3118
mbriese <mindi.briese@gmail.com>
2020-03-10 13:12:57 -0700

    fixing changes for post edit

--- router.js
+++ router.js
@@ -3,19 +3,19 @@ const router = express.Router()
 const userController = require('./controllers/userController')
 const postController = require('./controllers/postController')
 
-// User related routes
+// user related routes
 router.get('/', userController.home)
 router.post('/register', userController.register)
 router.post('/login', userController.login)
 router.post('/logout', userController.logout)
 
-// Profile related routes
+// profile related routes
 router.get('/profile/:username', userController.ifUserExists, userController.profilePostsScreen)
 
-
-// Post related routes
+// post related routes
 router.get('/create-post', userController.mustBeLoggedIn, postController.viewCreateScreen)
 router.post('/create-post', userController.mustBeLoggedIn, postController.create)
 router.get('/post/:id', postController.viewSingle)
+router.get('/post/:id/edit', postController.viewEditScreen)
 
 module.exports = router
