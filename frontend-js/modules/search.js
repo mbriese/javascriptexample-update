3cb71a176b0208a5c391c9920bf13cc5e8944043
mbriese <mindi.briese@gmail.com>
2020-03-11 16:30:34 -0700

    implementing Search

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -1,5 +1,22 @@
 export default class Search {
+    // Organization notes
+    // Select DOM elements, and keep track of any useful data
     constructor() {
-        alert("Search js is successfully being executed")
+        this.headerSearchIcon = document.querySelector(".header-search-icon")
+        this.events()
+    }
+
+    // Events
+    events() {
+        this.HeaderSearchIcon.addEventListener("click", (e) => {
+          e.preventDefault()
+          this.openOverlay()
+        })
+    }
+
+
+    // Methods
+    openOverlay() {
+        alert("openOverlay method just ran")
     }
 }
