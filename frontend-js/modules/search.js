45059404503da2a517b2bc737fba02b5e9e59ddf
mbriese <mindi.briese@gmail.com>
2020-03-12 17:16:44 -0700

    displaying search results

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import DOMpurify from 'dompurify'
 
 export default class Search {
     // 1. Select DOM elements, and keep track of any useful data
@@ -56,28 +57,18 @@ export default class Search {
 
    renderResultsHTML(posts) {
      if (posts.length) {
-        this.resultsArea.innerHTML = `<div class="list-group shadow-sm">
-        <div class="list-group-item active"><strong>Search Results</strong> (4 items found)</div>
-
-        <a href="#" class="list-group-item list-group-item-action">
-          <img class="avatar-tiny" src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"> <strong>Example Post #1</strong>
-          <span class="text-muted small">by barksalot on 0/14/2019</span>
-        </a>
-        <a href="#" class="list-group-item list-group-item-action">
-          <img class="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"> <strong>Example Post #2</strong>
-          <span class="text-muted small">by brad on 0/12/2019</span>
-        </a>
-        <a href="#" class="list-group-item list-group-item-action">
-          <img class="avatar-tiny" src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"> <strong>Example Post #3</strong>
-          <span class="text-muted small">by barksalot on 0/14/2019</span>
-        </a>
-        <a href="#" class="list-group-item list-group-item-action">
-          <img class="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"> <strong>Example Post #4</strong>
-          <span class="text-muted small">by brad on 0/12/2019</span>
-        </a>
-      </div>`
+        this.resultsArea.innerHTML = DOMpurify.sanitize(`<div class="list-group shadow-sm">
+        <div class="list-group-item active"><strong>Search Results</strong> (${posts.length > 1 ? `${posts.length} items found` : '1 item found' } )</div>
+         ${posts.map(post => {
+           let postDate = new Date(post.createdDate)
+           return `<a href="/post/${post._id}" class="list-group-item list-group-item-action">
+           <img class="avatar-tiny" src="${post.author.avatar}"> <strong>"${post.title}"</strong>
+           <span class="text-muted small">by ${post.author.username} on ${postDate.getMonth() + 1}/${postDate.getDate()}/${postDate.getFullYear()}</span>
+         </a>`
+         }).join('')}
+      </div>`)
      } else {
-      this.resultsArea.innerHTML = `<p class="alert alert-danger text-center shadow-sm">Sorry, we could not find any results for that search."</p>`
+      this.resultsArea.innerHTML = `<p class="alert alert-danger text-center shadow-sm">Sorry, we could not find any results for that search.</p>`
      }
       this.hideLoaderIcon()
       this.showResultsArea()
@@ -92,11 +83,11 @@ export default class Search {
     }
 
     showResultsArea() {
-       this.loaderIcon.classList.add("live-search-results--visible")
+       this.resultsArea.classList.add("live-search-results--visible")
     }
 
    hideResultsArea() {
-      this.loaderIcon.classList.remove("live-search-results--visible")
+      this.resultsArea.classList.remove("live-search-results--visible")
     }
     
     openOverlay() {
