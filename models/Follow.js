3fe2df649a035f7e8d2df30b8ff6faf97cc9aa90
mbriese <mindi.briese@gmail.com>
2020-03-13 13:24:59 -0700

    following user button action

--- models/Follow.js
+++ models/Follow.js
@@ -1,19 +1,37 @@
-let Follow = function() {
+const usersCollection = require('../db').db().collection("users")
+const followsCollection = require('../db').db().collection("follows")
+const ObjectID = require('mongodb').ObjectID
 
+let Follow = function(followedUsername, authorId) {
+  this.followedUsername = followedUsername
+  this.authorId = authorId
+  this.errors = []
 }
 
-Follow.prototype.cleanUp() {
-
+Follow.prototype.cleanUp = async function() {
+  if (typeof(this.followedUsername) != "string") {this.followedUsername = ""}
 }
 
-Follow.prototype.validate() {
-
+Follow.prototype.validate = async function() {
+ // followedUsername must exist in database
+  let followedAccount = await usersCollection.findOne({username: this.followedUsername})
+  if (followedAccount) {
+    this.followedId = followedAccount._id
+  } else {
+    this.errors.push("You cannot follow a user that does not exist.")
+  }
 }
 
 Follow.prototype.create = function() {
-   return new Promise((resolve, reject) => {
+   return new Promise(async (resolve, reject) => {
        this.cleanUp()
-       this.validate()
+       await this.validate()
+       if (!this.errors.length) {
+         await followsCollection.insertOne({followedId: this.followedId, authorId: new ObjectID(this.authorId)})
+         resolve()
+        } else {
+         reject(this.errors)
+       }
    })
 }
 
