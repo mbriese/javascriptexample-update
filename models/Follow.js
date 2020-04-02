8a38ff8a0cacec6d22ad1374f7ddc20caacca492
mbriese <mindi.briese@gmail.com>
2020-04-01 22:09:37 -0700

    adding counts to profile screen

--- models/Follow.js
+++ models/Follow.js
@@ -95,7 +95,7 @@ Follow.getFollowersById = function(id) {
 Follow.getFollowingById = function(id) {
   return new Promise(async (resolve, reject) => {
     try {
-      let following = await followsCollection.aggregate([
+      let followers = await followsCollection.aggregate([
         {$match: {authorId: id}},
         {$lookup: {from: "users", localField: "followedId", foreignField: "_id", as: "userDoc"}},
         {$project: {
@@ -103,14 +103,28 @@ Follow.getFollowingById = function(id) {
           email: {$arrayElemAt: ["$userDoc.email", 0]}
         }}
       ]).toArray()
-      following = followers.map(function(following) {
+      followers = followers.map(function(follower) {
         let user = new User(follower, true)
         return {username: follower.username, avatar: user.avatar}
       })
-      resolve(following)
+      resolve(followers)
     } catch {
       reject()
     }
+  })
+}
+
+Follow.countFollowersById() = function(id){
+  return new Promise(async (resolve, reject) => {
+    let followerCount = await followerCollection.countDocuments({followedId: id})
+    resolve(followerCount)
+  })
+}
+
+Follow.countFolloweringById() = function(id){
+  return new Promise(async (resolve, reject) => {
+    let count = await followingCollection.countDocuments({authorId: id})
+    resolve(count)
   })
 }
 
